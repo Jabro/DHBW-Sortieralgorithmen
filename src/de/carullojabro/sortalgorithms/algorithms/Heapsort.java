e71fb025f2a1330f75486856ceb38a715410e3de
Jan B <jabro91@gmx.de>
2012-06-20 12:37:46 +0200

    Testklassenstruktur

--- src/de/carullojabro/sortalgorithms/algorithms/Heapsort.java
+++ src/de/carullojabro/sortalgorithms/algorithms/Heapsort.java
@@ -2,9 +2,12 @@ package de.carullojabro.sortalgorithms.algorithms;
 
 import java.util.List;
 
-public class Heapsort implements ISortAlgorithm {
+public class Heapsort {
 
-	@Override
+	public Heapsort (){
+		
+	}
+	
 	public List<Integer> sort(List<Integer> list) {
 		// TODO Auto-generated method stub
 		return null;
---
+++ src/de/carullojabro/sortalgorithms/test/AlgorithmTest.java
@@ -0,0 +1,10 @@
+package de.carullojabro.sortalgorithms.test;
+
+import de.carullojabro.sortalgorithms.algorithms.Heapsort;
+import de.carullojabro.sortalgorithms.algorithms.ISortAlgorithm;
+
+public class AlgorithmTest {
+	public static void main(String[] args) {
+		
+	}
+}
