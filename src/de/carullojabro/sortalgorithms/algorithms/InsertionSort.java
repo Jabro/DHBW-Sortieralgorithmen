0823c42cfd02e3ea79ee8d0ae89718c2f295415b
Lukas Carullo <lukas.carullo@gmail.com>
2012-06-20 13:05:28 +0200

    Insertionsort implementiert

--- src/de/carullojabro/sortalgorithms/algorithms/InsertionSort.java
+++ src/de/carullojabro/sortalgorithms/algorithms/InsertionSort.java
@@ -5,15 +5,21 @@ import java.util.List;
 /**
  * 
  * @author Lukas
- *
+ * 
  */
 
 public class InsertionSort implements ISortAlgorithm {
 
-	@Override
 	public List<Integer> sort(List<Integer> list) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int i = 1; i < list.size(); i++) {
+			int x = list.get(i);
+			int j = i;
+			while (j > 0 && list.get(j - 1) > x) {
+				list.set(j, list.get(j - 1));
+				j = j - 1;
+			}
+			list.set(j, x);
+		}
+		return list;
 	}
-
 }
