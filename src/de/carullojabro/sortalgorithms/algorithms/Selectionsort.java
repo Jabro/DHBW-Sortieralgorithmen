071f6422f0d04750f0530e5981ecb120b3d34bed
Lukas Carullo <lukas.carullo@gmail.com>
2012-06-20 13:15:00 +0200

    Selectionsort

--- src/de/carullojabro/sortalgorithms/algorithms/Selectionsort.java
+++ src/de/carullojabro/sortalgorithms/algorithms/Selectionsort.java
@@ -6,8 +6,18 @@ public class Selectionsort implements ISortAlgorithm {
 
 	@Override
 	public List<Integer> sort(List<Integer> list) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int i = 0; i < list.size() - 2; i++) {
+			int k = i;
+			for (int j = i + 1; j < list.size() - 1; j++) {
+				if (list.get(j) < list.get(k)) {
+					k = j;
+				}
+				int x = list.get(k);
+				list.set(k, list.get(i));
+				list.set(i, x);
+			}
+		}
+		return list;
 	}
 
 }
