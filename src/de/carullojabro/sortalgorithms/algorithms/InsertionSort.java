c8d5ddf1d10dc7952d1fa6ae8a6fb1dbc2dc6a5e
Jan B <jabro91@gmx.de>
2012-06-20 12:25:06 +0200

    Klassenstruktur erstellt

---
+++ src/de/carullojabro/sortalgorithms/algorithms/InsertionSort.java
@@ -0,0 +1,13 @@
+package de.carullojabro.sortalgorithms.algorithms;
+
+import java.util.List;
+
+public class InsertionSort implements ISortAlgorithm {
+
+	@Override
+	public List<Integer> sort(List<Integer> list) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
