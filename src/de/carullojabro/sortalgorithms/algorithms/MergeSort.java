2e4179d2263cda872a64e9980513a3a02e5e3351
Lukas Carullo <lukas.carullo@gmail.com>
2012-07-02 18:45:00 +0200

    Changes fpr runtimetests

---
+++ src/de/carullojabro/sortalgorithms/algorithms/MergeSort.java
@@ -0,0 +1,94 @@
+package de.carullojabro.sortalgorithms.algorithms;
+
+public class MergeSort {
+
+	public int[] sort(int[] list) {
+
+		int oldLength = list.length;
+		int potenz = (int) Math.ceil(Math.log(oldLength) / Math.log(2));
+		int length = (int) Math.pow(2, potenz);
+		int[] listA = new int[length];
+		int[] listB = new int[length / 2];
+		int[] listC = new int[length];
+		int[] listD = new int[length / 2];
+
+		for (int i = 0; i < oldLength; i++) {
+			if (i % 2 == 0) {
+				listC[i / 2] = list[i];
+			} else {
+				listD[i / 2] = list[i];
+			}
+		}
+		for (int i = oldLength; i < length; i++) {
+			if (i % 2 == 0) {
+				listC[i / 2] = Integer.MAX_VALUE;
+			} else {
+				listD[i / 2] = Integer.MAX_VALUE;
+			}
+		}
+		boolean odd = true;
+		for (int blockSize = 1; blockSize <= listA.length / 2; blockSize *= 2) {
+			if (odd) {
+				merge(listC, listD, listA, listB, blockSize);
+			} else {
+				merge(listA, listB, listC, listD, blockSize);
+			}
+			odd = !odd;
+		}
+
+		if (odd) {
+			for (int i = 0; i < oldLength; i++) {
+				list[i] = listC[i];
+			}
+		} else {
+			for (int i = 0; i < oldLength; i++) {
+				list[i] = listA[i];
+			}
+		}
+
+		return list;
+
+	}
+
+	private void merge(int[] listA, int[] listB, int[] listC, int[] listD,
+			int blockSize) {
+
+		for (int blockOffset = 0; blockOffset * blockSize <= listA.length / 2; blockOffset++) {
+			if (blockOffset % 2 == 1) {
+				mergeBlock(listA, listB, listD, blockOffset, blockSize);
+			} else {
+				mergeBlock(listA, listB, listC, blockOffset, blockSize);
+			}
+		}
+
+	}
+
+	private void mergeBlock(int[] listA, int[] listB, int[] listC,
+			int blockOffset, int blockSize) {
+
+		int a = blockOffset * blockSize;
+		int b = a;
+		int c = blockOffset / 2 * 2 * blockSize;
+		int nextOffset = (blockOffset + 1) * blockSize;
+		while (a < nextOffset && b < nextOffset && a < listA.length
+				&& b < listB.length) {
+			if (listA[a] <= listB[b]) {
+				listC[c++] = listA[a];
+				a++;
+			} else {
+				listC[c++] = listB[b];
+				b++;
+			}
+		}
+		while (a < nextOffset && a < listA.length) {
+			listC[c++] = listA[a];
+			a++;
+		}
+		while (b < nextOffset && b < listB.length) {
+			listC[c++] = listB[b];
+			b++;
+		}
+
+	}
+
+}
--- src/de/carullojabro/sortalgorithms/test/AlgorithmTest.java
+++ src/de/carullojabro/sortalgorithms/test/AlgorithmTest.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import de.carullojabro.sortalgorithms.algorithms.BubbleSort;
 import de.carullojabro.sortalgorithms.algorithms.Bucketsort;
 import de.carullojabro.sortalgorithms.algorithms.Heapsort;
 import de.carullojabro.sortalgorithms.algorithms.InsertionSort;
+import de.carullojabro.sortalgorithms.algorithms.MergeSort;
 import de.carullojabro.sortalgorithms.algorithms.Quicksort;
 import de.carullojabro.sortalgorithms.algorithms.Selectionsort;
 import de.carullojabro.sortalgorithms.algorithms.Shellsort;
@@ -26,99 +28,172 @@ public class AlgorithmTest {
 	 * z.B. 100000
 	 */
 	public static void main(String[] args) {
+		double a, b, c, d, e;
+		// System.out.println("Test");
+		// testSort(1, 10000);
 
-		sortInsertionSort(0, 1000);
-		sortInsertionSort(0, 1000);
-		sortInsertionSort(0, 1000);
-		sortInsertionSort(0, 10000);
-		sortInsertionSort(0, 10000);
-		sortInsertionSort(0, 10000);
-		sortInsertionSort(0, 100000);
-		sortInsertionSort(0, 100000);
-		sortInsertionSort(0, 100000);
-		
-		sortInsertionSort(1, 1000);
-		sortInsertionSort(1, 1000);
-		sortInsertionSort(1, 1000);
-		sortInsertionSort(1, 10000);
-		sortInsertionSort(1, 10000);
-		sortInsertionSort(1, 10000);
-		sortInsertionSort(1, 100000);
-		sortInsertionSort(1, 100000);
-		sortInsertionSort(1, 100000);
-		// sortSelectionSort(0, 100000);
-		// sortBubbleSort(0, 100000);
-		// TODO shellsort noch implementieren
+		System.out.println("Sorted");
+		a = mergeSort(0, 1000);
+		b = mergeSort(0, 1000);
+		c = mergeSort(0, 1000);
+		System.out.println((a + b + c) / 3);
+		a = mergeSort(0, 10000);
+		b = mergeSort(0, 10000);
+		c = mergeSort(0, 10000);
+		System.out.println((a + b + c) / 3);
+		a = mergeSort(0, 100000);
+		b = mergeSort(0, 100000);
+		c = mergeSort(0, 100000);
+		System.out.println((a + b + c) / 3);
+		System.out.println("Invers");
+		a = mergeSort(1, 1000);
+		b = mergeSort(1, 1000);
+		c = mergeSort(1, 1000);
+		System.out.println((a + b + c) / 3);
+		a = mergeSort(1, 10000);
+		b = mergeSort(1, 10000);
+		c = mergeSort(1, 10000);
+		System.out.println((a + b + c) / 3);
+		a = mergeSort(1, 100000);
+		b = mergeSort(1, 100000);
+		c = mergeSort(1, 100000);
+		System.out.println((a + b + c) / 3);
+		System.out.println("Random");
+		a = mergeSort(2, 1000);
+		b = mergeSort(2, 1000);
+		c = mergeSort(2, 1000);
+		d = mergeSort(2, 1000);
+		e = mergeSort(2, 1000);
+		System.out.println((a + b + c + d + e) / 5);
+		a = mergeSort(2, 10000);
+		b = mergeSort(2, 10000);
+		c = mergeSort(2, 10000);
+		d = mergeSort(2, 10000);
+		e = mergeSort(2, 10000);
+		System.out.println((a + b + c + d + e) / 5);
+		a = mergeSort(2, 100000);
+		b = mergeSort(2, 100000);
+		c = mergeSort(2, 100000);
+		d = mergeSort(2, 100000);
+		e = mergeSort(2, 100000);
+		System.out.println((a + b + c + d + e) / 5);
 		// sortHeapSort(0, 100000);
-		// sortQuickSort(0, 100000);
-		// ausgleichendes Mischen noch implementieren
-		// TODO List<Integer> list = createRandomList(100000);
-		//sortShellSort(2, 10000);
+		// sotieren durch mischen
+		// sortShellSort(2, 10000);
 	}
 
-	private static void sortBucketSort(int index, int n) {
+	private static double mergeSort(int index, int n) {
+		int[] list = new int[n];
+		switch (index) {
+		case 0: {
+			for (int i = 0; i < list.length; i++) {
+				list[i] = i;
+			}
+			break;
+		}
+		case 1: {
+			for (int i = list.length - 1; i == 0; i--) {
+				list[i] = i;
+			}
+			break;
+		}
+		case 2: {
+			Random rdm = new Random();
+			for (int i = 0; i < list.length; i++) {
+				list[i] = rdm.nextInt(n);
+			}
+			break;
+		}
+
+		default:
+			break;
+		}
+
+		// Sortieren
+		MergeSort mS = new MergeSort();
+		double start = System.currentTimeMillis();
+		list = mS.sort(list);
+		double end = System.currentTimeMillis();
+		System.out.println("MergeSort mit " + n + "Elementen Listenoption "
+				+ index + " " + (end - start) / 1000 + " Sekunden benötigt.");
+		return (end - start) / 1000;
+	}
+
+	private static double sortBucketSort(int index, int n) {
 		List<Integer> list = createListBucketM10(index, n);
 		Bucketsort oSort = new Bucketsort(list);
 		double start = System.currentTimeMillis();
 		list = oSort.sort();
 		double end = System.currentTimeMillis();
-		System.out.println("BucketSort mit"+n+"Elementen Listenoption "+index+" "+(end - start) / 1000 + " Sekunden benötigt.");
+		System.out.println("BucketSort mit" + n + "Elementen Listenoption "
+				+ index + " " + (end - start) / 1000 + " Sekunden benötigt.");
+		return (end - start) / 1000;
 	}
 
-	private static void sortQuickSort(int index, int n) {
+	private static double sortQuickSort(int index, int n) {
 		List<Integer> list = createCustomList(index, n);
 		Quicksort oSort = new Quicksort(list);
 		double start = System.currentTimeMillis();
 		list = oSort.sort();
 		double end = System.currentTimeMillis();
-		System.out.println("QuickSort mit"+n+"Elementen Listenoption "+index+" "+(end - start) / 1000 + " Sekunden benötigt.");
+		System.out.println("QuickSort mit" + n + "Elementen Listenoption "
+				+ index + " " + (end - start) / 1000 + " Sekunden benötigt.");
+		return (end - start) / 1000;
 	}
 
-	private static void sortHeapSort(int index, int n) {
+	private static double sortHeapSort(int index, int n) {
 		List<Integer> list = createCustomList(index, n);
 		Heapsort oSort = new Heapsort(list);
 		double start = System.currentTimeMillis();
 		list = oSort.sort();
 		double end = System.currentTimeMillis();
-		System.out.println("HeapSort mit"+n+"Elementen Listenoption "+index+" "+(end - start) / 1000 + " Sekunden benötigt.");
+		System.out.println("HeapSort mit" + n + "Elementen Listenoption "
+				+ index + " " + (end - start) / 1000 + " Sekunden benötigt.");
+		return (end - start) / 1000;
 	}
 
-	private static void sortBubbleSort(int index, int n) {
+	private static double sortBubbleSort(int index, int n) {
 		List<Integer> list = createCustomList(index, n);
-		Bucketsort oSort = new Bucketsort(list);
+		BubbleSort oSort = new BubbleSort(list);
 		double start = System.currentTimeMillis();
 		list = oSort.sort();
 		double end = System.currentTimeMillis();
-		System.out.println("BubbleSort mit"+n+"Elementen Listenoption "+index+" "+(end - start) / 1000 + " Sekunden benötigt.");
+		System.out.println("BubbleSort mit" + n + "Elementen Listenoption "
+				+ index + " " + (end - start) / 1000 + " Sekunden benötigt.");
+		return (end - start) / 1000;
 	}
 
-	private static void sortSelectionSort(int index, int n) {
+	private static double sortSelectionSort(int index, int n) {
 		List<Integer> list = createCustomList(index, n);
 		Selectionsort oSort = new Selectionsort(list);
 		double start = System.currentTimeMillis();
 		list = oSort.sort();
 		double end = System.currentTimeMillis();
-		System.out.println("SelectionSort mit"+n+"Elementen Listenoption "+index+" "+(end - start) / 1000 + " Sekunden benötigt.");
+		System.out.println("SelectionSort mit" + n + "Elementen Listenoption "
+				+ index + " " + (end - start) / 1000 + " Sekunden benötigt.");
+		return (end - start) / 1000;
 	}
 
-	private static void sortInsertionSort(int index, int n) {
+	private static double sortInsertionSort(int index, int n) {
 		List<Integer> list = createCustomList(index, n);
 		InsertionSort oSort = new InsertionSort(list);
 		double start = System.currentTimeMillis();
 		list = oSort.sort();
 		double end = System.currentTimeMillis();
-		System.out.println("InsertionSort mit"+n+"Elementen Listenoption "+index+" "+(end - start) / 1000 + " Sekunden benötigt.");
+		System.out.println("InsertionSort mit" + n + "Elementen Listenoption "
+				+ index + " " + (end - start) / 1000 + " Sekunden benötigt.");
+		return (end - start) / 1000;
 	}
 
-	private static void sortShellSort(int index, int n) {
+	private static double sortShellSort(int index, int n) {
 		List<Integer> list = createCustomList(index, n);
 		Shellsort oSort = new Shellsort(list);
 		double start = System.currentTimeMillis();
-		System.out.println(list);
 		list = oSort.sort();
 		double end = System.currentTimeMillis();
-		System.out.println(list);
-		System.out.println("ShellSort mit"+n+"Elementen Listenoption "+index+" "+(end - start) / 1000 + " Sekunden benötigt.");
+		System.out.println("ShellSort mit" + n + "Elementen Listenoption "
+				+ index + " " + (end - start) / 1000 + " Sekunden benötigt.");
+		return (end - start) / 1000;
 	}
 
 	private static List<Integer> createCustomList(int index, int n) {
